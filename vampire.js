b71e1a8dba51dd91650727e81d63933bf194ec28
kcdporter <kiracdporter@gmail.com>
2019-04-06 03:26:50 +0000

    all functions built EXCEPT stretch

--- vampire.js
+++ vampire.js
@@ -10,24 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    if (this.creator === null) {
+      return 0
+    } else {
+      return 1 + this.creator.numberOfVampiresFromOriginal;
+    }
+  }
 
+  get lineage() {
+    if (this.creator === null) {
+      return this;
+    } else {
+      return [this.creator].concat(this.creator.lineage);
+    }
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
-
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
